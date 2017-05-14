01cee8a5f36576d7cc75bd6ccbdda6fc3cf70a03
Jose Nunez <josenunezdiaz@gmail.com>
2017-05-15 02:20:44 +1100

    First commit

---
+++ src/index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+<?php wp_head(); ?>
+</head>
+<body>
+	<section id="content" role="main">
+		<div class="maintitle">Joanne Crowther</div>
+	</section>
+</body>	
+</html>
