4bdb308b98df6a3daf67eeb2a5b3a1653e458461
Jose Nunez <josenunezdiaz@gmail.com>
2017-06-09 20:53:46 +1100

    New comments - first stage for LISA

--- src/comments.php
+++ src/comments.php
@@ -36,6 +36,14 @@ $ping_count = count( $comments_by_type['pings'] );
 <?php 
 endif; 
 endif;
-if ( comments_open() ) comment_form();
+if ( comments_open() ){
+
+	$comment_opt = array(
+		// 'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment here', 'noun' ) . '</label><textarea placeholder="HOLA LISA MUACK MUACK" id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>'
+		'comment_field' => '<p class="comment-form-comment"><label for="comment">' . '</label><textarea placeholder="'. _x( 'Comment here', 'noun' ) .'" id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>'
+	);
+
+	comment_form($comment_opt);
+}
 ?>
 </section>
