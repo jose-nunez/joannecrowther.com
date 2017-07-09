d2f6b0fe747bcb1524ba8ab21f1058325f6f7f47
Jose Nunez <josenunezdiaz@gmail.com>
2017-07-09 23:13:43 +1000

    Cleaned Grunge background code

--- src/entry.php
+++ src/entry.php
@@ -10,19 +10,9 @@
 		?>
 			<div class="thumbnail" style="background-image:url(<?php echo get_template_directory_uri() ?>/img/frame_1_white.png),url(<?php echo get_the_post_thumbnail_url(); ?>);<?php echo $bkgpos? $bkgpos:''; ?>"></div>	
 		<?php endif; ?>
-
-		
-		<?php 
-			echo get_the_term_list($post->ID,'compilation','<h2 class="entry-compilation hidden">','','</h2>'); 
-		?>
-	
-		<h1 class="entry-title">
-		<?php /*
-			<a class="hidden" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
-		*/ ?>
-			<a class="" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
-		</h1>
-		<?php //edit_post_link(); ?>
+		<?php echo get_the_term_list($post->ID,'compilation','<h2 class="entry-compilation hidden">','','</h2>'); ?>
+		<h1 class="entry-title"><a class="" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<?php edit_post_link(); ?>
 		<?php //if ( !is_search() ) get_template_part( 'entry', 'meta' ); ?>
 	</header>
 	<?php get_template_part( 'entry', ( is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
