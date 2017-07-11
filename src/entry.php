8fe8e12f9b75e7b87ca616087ecc1499a2160d0b
Jose Nunez <josenunezdiaz@gmail.com>
2017-07-12 03:14:48 +1000

    Styling Entry Title

--- src/entry.php
+++ src/entry.php
@@ -11,7 +11,7 @@
 			<div class="thumbnail" style="background-image:url(<?php echo get_template_directory_uri() ?>/img/frame_1_white.png),url(<?php echo get_the_post_thumbnail_url(); ?>);<?php echo $bkgpos? $bkgpos:''; ?>"></div>	
 		<?php endif; ?>
 		<?php echo get_the_term_list($post->ID,'compilation','<h2 class="entry-compilation hidden">','','</h2>'); ?>
-		<h1 class="entry-title"><a class="" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<h1 class="entry-title"><a class="hidden" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 		<?php edit_post_link(); ?>
 		<?php //if ( !is_search() ) get_template_part( 'entry', 'meta' ); ?>
 	</header>
