7b288e3a1965506a7937f582ab7d381f91c564ea
Jose Nunez <josenunezdiaz@gmail.com>
2017-07-15 05:35:10 +1000

    Restyling Nav-Below-Attachemnt

--- src/nav-below-attachment.php
+++ src/nav-below-attachment.php
@@ -1,5 +1,5 @@
 
 <nav id="nav-below" class="navigation clearfix" role="navigation">
-	<div class="nav-previous"><?php previous_image_link( false, '&larr;' ); ?></div>
-	<div class="nav-next"><?php next_image_link( false, '&rarr;' ); ?></div>
+	<div class="nav-previous"><?php previous_image_link( false, '&larr; Previous Image' ); ?></div>
+	<div class="nav-next"><?php next_image_link( false, 'Next Image &rarr;' ); ?></div>
 </nav>
