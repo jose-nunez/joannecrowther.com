8a702f017c639bcc457da844e8b975a4405dae0b
Jose Nunez <josenunezdiaz@gmail.com>
2017-07-16 05:51:31 +1000

    Category archives

--- src/category.php
+++ src/category.php
@@ -1,13 +1,22 @@
 <?php get_header(); ?>
-<section id="content" role="main">
-<header class="header">
-<h1 class="entry-title"><?php _e( 'Category Archives: ', 'joannecrowther' ); ?><?php single_cat_title(); ?></h1>
-<?php if ( '' != category_description() ) echo apply_filters( 'archive_meta', '<div class="archive-meta">' . category_description() . '</div>' ); ?>
-</header>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
-</section>
-<?php get_sidebar(); ?>
+<div id="container" class="right-sidebar clearfix">
+	<section id="content" role="main">
+		<div class="content-inner">
+			<header class="header">
+				<h1 class="archive-title"><?php _e( 'Category Archives: ', 'joannecrowther' ); ?><?php single_cat_title(); ?></h1>
+
+
+
+
+
+			</header>			
+			<?php if ( '' != category_description() ) echo apply_filters( 'archive_meta', '<div class="archive-meta">' . category_description() . '</div>' ); ?>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'partials/summary' ); ?>
+			<?php endwhile; endif; ?>
+			<?php get_template_part( 'nav', 'below' ); ?>
+		</div>
+	</section>
+	<?php get_sidebar(); ?>
+</div>
 <?php get_footer(); ?>
